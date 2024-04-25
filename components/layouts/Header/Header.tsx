741e0e89763fcda8cdd222776a6e7f6386234882
rintaro1029 <tomitaka1029@gmail.com>
2024-04-26 01:27:56 +0900

    create normal header

--- components/layouts/Header/Header.tsx
+++ components/layouts/Header/Header.tsx
@@ -1,14 +1,20 @@
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { Settings } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between px-12 py-3 bg-orange-200 shadow-sm dark:bg-gray-900">
+    <header className="h-20 flex items-center justify-between px-12 py-3 bg-orange-200 shadow-sm dark:bg-gray-900">
       <div> </div>
-      <Link href="/" className="text-2xl font-medium text-orange-900 dark:text-gray-100">
-        大阪おすすめグルメ
-      </Link>
+      <div className="flex space-x-2 items-center">
+        <Image src="/food/tako.png" alt="食べ物" width={60} height={60} />
+        <Link href="/" className="text-2xl font-medium text-orange-900 dark:text-gray-100">
+          大阪おすすめグルメ
+        </Link>
+      </div>
+
       <div className="flex items-center space-x-4">
         {" "}
         {/* グルメ、ログイン、登録のリンクを右端に配置 */}
@@ -39,6 +45,7 @@ const Header = () => {
         >
           Register
         </Link>
+        <Settings />
       </div>
     </header>
   );
